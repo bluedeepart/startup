6bbc7a3eb4dc07c5a3ad00704662323a5ddf4bc3
Deepti Rathor <31531837+bluedeepart@users.noreply.github.com>
2024-02-05 12:42:49 +0530

    Added menu link and smooth scrolling

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,19 @@ window.addEventListener('DOMContentLoaded', () => {
   const megaMenuBtn = document.getElementById('mega_menu_btn');
   const header = document.querySelector('.header');
 
+  /* SMOOTH SCROLLING */
+  const menuItems = document.querySelectorAll('.header_nav a');
+  menuItems.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const sectionID = new URL(link.href).hash;
+      let section = document.querySelector(sectionID);
+      section.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
   /* MOB MENU */
   megaMenuBtn.addEventListener('click', (event) => {
     event.preventDefault();
