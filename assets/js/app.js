4e2bedc42bc6b58d64d957eb5afba8c8312b1ca5
Deepti Rathor <31531837+bluedeepart@users.noreply.github.com>
2024-02-11 03:09:16 +0530

    fixed: mob menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
   const backToTop = document.getElementById('back_to_top');
   const megaMenuBtn = document.getElementById('mega_menu_btn');
+  const megaMenuCloseBtn = document.getElementById('mega_menu_close_btn');
   const header = document.querySelector('.header');
 
   /* SMOOTH SCROLLING */
@@ -17,7 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   /* MOB MENU */
-  megaMenuBtn.addEventListener('click', (event) => {
+  function handleMobMenu(event) {
     event.preventDefault();
 
     if (header.classList.contains('header-open')) {
@@ -29,7 +30,9 @@ window.addEventListener('DOMContentLoaded', () => {
       document.body.style.overflowY = 'hidden';
       document.body.style.paddingRight = '17px';
     }
-  });
+  }
+  megaMenuBtn.addEventListener('click', handleMobMenu);
+  megaMenuCloseBtn.addEventListener('click', handleMobMenu);
 
   /* BACK TO TOP */
   backToTop.addEventListener('click', handleTop);
