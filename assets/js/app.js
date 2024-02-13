fa6babaf305f0fd779ebebe0406653d5f4c0060a
Deepti Rathor <31531837+bluedeepart@users.noreply.github.com>
2024-02-13 10:31:05 +0530

    button removed on menu open

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,16 +32,10 @@ window.addEventListener('DOMContentLoaded', () => {
       header.classList.remove('header-open');
       document.body.style.overflowY = 'auto';
       document.body.style.paddingRight = '0';
-
-      header.querySelector('.container').style.overflowY = 'hidden';
-      header.querySelector('.container').style.height = 'auto';
     } else {
       header.classList.add('header-open');
       document.body.style.overflowY = 'hidden';
       document.body.style.paddingRight = '17px';
-
-      header.querySelector('.container').style.overflowY = 'auto';
-      header.querySelector('.container').style.height = '100%';
     }
   }
   megaMenuBtn.addEventListener('click', handleMobMenu);
